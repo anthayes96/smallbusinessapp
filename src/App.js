13c322a68166ca6b46c664620bc9d8ad02c7948e
anthayes96 <ant.hayes96@gmail.com>
2020-05-03 12:23:24 -0500

    fixed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React from 'react';
-import Navigation from './components/Navigation'
-import './App.css'
-import Router from '.src/Router'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './redux/store'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Navigation />
+    <div className="App">
       
-        <Router />
-      </BrowserRouter>
-    </Provider>
+       
+    </div>
   );
 }
 
